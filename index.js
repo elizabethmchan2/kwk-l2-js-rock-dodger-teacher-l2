1af2d28fc8c103a37d2e1fcb9dd61479650a238f
Elizabeth Chan <lmchan@brynmawr.edu>
2018-08-09 17:48:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -80,7 +80,7 @@ function createRock(x) {
     
     // var top = 0;
     function step() {
-      rock.style.top = `${top += 2}px`
+      rock.style.marginTop = `${top += 2}px`
    
       if (top < 200) {
         window.requestAnimationFrame(step)
