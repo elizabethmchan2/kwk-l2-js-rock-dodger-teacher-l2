529cf33e89fa1f2aeb74d5ceb5ad513bfd0125c3
Elizabeth Chan <lmchan@brynmawr.edu>
2018-08-09 17:49:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -108,6 +108,7 @@ function createRock(x) {
 
   // Add the rock to ROCKS so that we can remove all rocks
   // when there's a collision
+  moveRock();
   ROCKS.push(rock);
 
   // Finally, return the rock element you've created
